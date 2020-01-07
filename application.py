140e8f5a38b46122d0be4c917e5a19ff744731c6
Daniel Moraite <45300170+DanielMoraite@users.noreply.github.com>
2020-01-07 22:51:05 +0200

    Update application.py

--- application.py
+++ application.py
@@ -1,10 +1,7 @@
 from flask import Flask, request, render_template
-
-import os
-from glob import glob
-import pandas as pd
-import numpy as np
 from werkzeug import secure_filename
+import os
+
 from commons import data_mfcc
 from commons import predict_func
 from commons import model_load_compile
@@ -28,19 +25,15 @@ def upload_file():
             return '[server] no file part'
 
         f = request.files['file']
-
         if f.filename == '':
             application.logger.debug("[server] empty file")
             return '[server] empty file'
 
         filename = secure_filename(f.filename)
-
         global temp_last_filename
         temp_last_filename = filename
-        print(f"numele creat este: {temp_last_filename}")
 
         f.save(os.path.join(application.root_path,'uploads', filename))
-
         application.logger.debug("[server] file uploaded")
         return '[server] file uploaded'
 
@@ -50,7 +43,6 @@ def predict_button():
         application.logger.debug("[server] predict sentiment")
         #get file name
         filename = './uploads/' + temp_last_filename
-        print(f"adresa citita este: {filename}")
         #Take the file name and load it into python for mfcc and prediction
         newdf = data_mfcc(filename)  # MFCC The Newest File
         pred_res = predict_func(model, lb, newdf)
